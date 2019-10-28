22eccb900bdb24bf17b122d2c55fac860eb318f8
Sam Warren <sjwarren91@gmail.com>
2019-10-28 20:20:12 +1100

    add book schema and database connection

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Navbar() {
+  return (
+    <div className="header">
+      <div className="logo"></div>
+      <div className="links">
+        
+      </div>
+    </div>
+  )
+}
---
+++ models/book.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  title: { type: String, required: true },
+  authors: { type: String, required: true },
+  description: { type: String},
+  image: String,
+  link: String 
+});
