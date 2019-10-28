4f6bd6162abc13f9bd6eec39604e570db2dceaa1
Sam Warren <sjwarren91@gmail.com>
2019-10-28 21:18:38 +1100

    add react components navbar and jumbotron, add styling

---
+++ client/src/components/Jumbotron.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Jumbotron.css"
+
+function Jumbotron() {
+  return (
+    <div className="jumbotron"></div>
+  )
+}
+
+export default Jumbotron
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,20 @@
-import React from "react"
+import React from "react";
+import "./Navbar.css";
 
 function Navbar() {
   return (
     <div className="header">
-      <div className="logo"></div>
-      <div className="links">
-        
+      <div className="container">
+        <div className="logo">
+          <img src="./Google-Books.png" alt="Logo" />
+        </div>
+        <div className="links">
+          <a href="/">Search</a>
+          <a href="/">Saved</a>
+        </div>
       </div>
     </div>
-  )
+  );
 }
+
+export default Navbar;
